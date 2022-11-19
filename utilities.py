eb9546ca93c69f0196df3c76792a30d43b578a6a
Till Wenke <till.wenke@gmx.net>
2022-11-19 14:58:10 +0200

    extract translation code

---
+++ utilities.py
@@ -0,0 +1,20 @@
+# Imports
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from deep_translator import GoogleTranslator
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_colwidth", None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_seq_items', None)
+
+
+# dtype={'type': str} prevents being confused with data type for large data sets
+train = pd.read_csv('data/train.csv', index_col='id', dtype={'type': str})
+test = pd.read_csv('data/test.csv', index_col='id', dtype={'type': str})
+train_translated = pd.read_csv('data/train_translated.csv', dtype={'type': str})
+test_translated = pd.read_csv('data/test_translated.csv', index_col='id', dtype={'type': str})
+combined_data = pd.read_csv('data/combined_data.csv', index_col='id', dtype={'type': str})
+combined_data_translated = pd.read_csv('data/combined_data_translated.csv', index_col='id', dtype={'type': str})
+combined_data_fully_translated = pd.read_csv('data/combined_data_fully_translated.csv', index_col='id', dtype={'type': str})
