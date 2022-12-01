8eac39e25b60a8d86e4c5eb17f2289ec76e83b5d
Till Wenke <till.wenke@gmx.net>
2022-12-01 23:37:19 +0200

    first steps wandb sweeps

--- utilities.py
+++ utilities.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from deep_translator import GoogleTranslator
+import wandb
 
 pd.set_option("display.max_columns", None)
 pd.set_option("display.max_colwidth", None)
@@ -18,3 +19,4 @@ test_translated = pd.read_csv('data/test_translated.csv', index_col='id', dtype=
 combined_data = pd.read_csv('data/combined_data.csv', index_col='id', dtype={'type': str})
 combined_data_translated = pd.read_csv('data/combined_data_translated.csv', index_col='id', dtype={'type': str})
 combined_data_fully_translated = pd.read_csv('data/combined_data_fully_translated.csv', index_col='id', dtype={'type': str})
+prep = pd.read_csv('data/prep.csv', index_col='id', dtype={'type': str})
