9a52581e4e39bbc2c72d46413d7720b9fe0a7316
Till Wenke <till.wenke@gmx.net>
2022-12-24 16:38:13 +0200

    big sweep intro

---
+++ prep_helpers.py
@@ -0,0 +1,178 @@
+import numpy as np
+import pandas as pd
+import re
+
+# Combine parameter, unit & w/h values to value
+def get_squared(item):
+    if ' x ' in item:
+        return item + '²'
+    else:
+        return item
+
+def extract_width_height_from_unit_to_value(item):
+    unit = item[0]
+    value = item[1]
+    if ' x ' in unit:
+        split = unit.split(' ')
+        x = split[0]
+        y = split[2]
+        real_unit = split[3]
+        real_value = [x,y]        
+        return [real_unit, real_value]
+
+    else:
+        return [unit, value]
+
+
+def extract_value(value, present):
+    if present == 1:
+        return value
+    else:
+        return 0
+
+# make all size features numeric
+def extract_height_width(item):    
+    if item == 0:
+        return [0.0,0.0]
+    else:        
+        return [float(i) for i in item]
+
+def empty_to_nan(item):
+    if type(item) == str:
+        item = item.strip()
+        if item == '':
+            return np.nan
+        else:
+            return item
+    else:
+        return item
+
+# country_unit
+def extract_city_country(item):      
+    if (type(item) == str):
+        item = item.strip()
+        # there are some empty (non-nan) values
+        if (item == ''):
+            return [float('nan'), float('nan')]
+    
+        item = re.sub(' +', ' ', item) # remove multiple spaces
+
+        if (' ' in item) and ('Eesti' in item):        
+            split = item.split(' ')
+            return [' '.join(split[1:]), split[0]]
+        else: 
+            return [float('nan'), item]
+    else:
+        return [float('nan'), float('nan')]
+
+#groups the year into its own column with the complete number, if NaN, then 0
+def year_Grouping(x):
+    xStr = str(x)
+    if xStr == 'nan':
+        return 0
+    if '.' in xStr:
+        xStr = xStr.split('.')
+        xStr = xStr[xStr.__len__()-1]
+        if xStr.__len__() == 2:
+            xStr = '19' + xStr
+        elif xStr == '':
+            return 0
+        return int(xStr)
+    else:
+        return int(xStr)
+
+# returns 1 if a month is given, 0 if not
+def month_Grouping(x):
+    xStr = str(x)
+    if '.' in xStr:
+        xStr = xStr.split('.')
+        if xStr[0].__len__() <= 2 and xStr[1].__len__() <= 2:
+            if xStr[1] == '':
+                return 0
+            elif xStr[1].__len__() == 1:
+                return 1
+            return 1
+        xStr = xStr[0]
+        if xStr == 'jaan':
+            return 1
+        elif xStr == 'veebr':
+            return 1
+        if xStr == 'märts':
+            return 1
+        elif xStr == 'apr':
+            return 1
+        elif xStr == 'mai':
+            return 1
+        elif xStr == 'juuni':
+            return 1
+        elif xStr == 'juuli':
+            return 1
+        elif xStr == 'aug':
+            return 1
+        elif xStr == 'sept':
+            return 1
+        elif xStr == 'okt':
+            return 1
+        elif xStr == 'nov':
+            return 1
+        elif xStr == 'dets':
+            return 1
+        elif xStr.__len__() == 1:
+            return 1
+        return 1
+    else:
+        return 0
+
+#returns one if a day is given, 0 if not
+def day_Grouping(x):
+    xStr = str(x)
+    if '.' in xStr:
+        xStr = xStr.split('.')
+        if xStr[0].__len__() <= 2 and xStr[1].__len__() <= 2:
+            return 1
+        return 0
+    else:
+        return 0
+
+def extract_year_from_name(row):
+    name = row['name']
+    start = row['start']
+    if pd.isnull(start) and not pd.isnull(name):
+        match = re.search('\d\d\d\d', name)
+        if match:
+            start = match.group()
+    return start
+
+def strip_brackets(x):
+    x = str(x)
+    x = x.strip('< >')
+    if '\u200b' in x:
+        x = x.replace('\u200b', '')
+    return x
+
+def colour_grouping(x):
+    if x in ['madara red', 'dark red', 'purple red', 'Red']:
+        return 'red'
+    elif x in ['light blue', 'dark blue', 'purple blue', 'greenish blue', 'greyish blue']:
+        return 'blue'
+    elif x in ['light green', 'light olive green', 'grey-green', 'olive green', 'greyish-olive green', 'dark green']:
+        return 'green'
+    elif x in ['bluish grey', 'dark grey', 'pinkish gray']:
+        return 'grey'
+    elif x in ['pale yellow', 'light yellow', 'orange-yellow', 'brilliant yellow']:
+        return 'yellow'
+    elif x in ['brownish orange']:
+        return 'orange'
+    elif x in ['light brown', 'dark brown', 'greyish brown', 'reddish brown', 'olive brown', 'yellowish brown']:
+        return 'brown'
+    elif x in ['yellowish white', 'bluish white']:
+        return 'white'
+    elif x in ['brownish black']:
+        return 'black'
+    elif x in ['mauve pink']:
+        return 'pink'
+    elif x in ['<patterned>', 'striped', 'checkered']:
+        return 'patterned'
+    else:
+        return x
+
--- setup_general.py
+++ setup_general.py
@@ -14,10 +14,12 @@ import pickle
 import openai
 from collections import Counter
 
-from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, cross_validate
 from sklearn.metrics import roc_auc_score, accuracy_score, classification_report
 from sklearn.preprocessing import LabelEncoder, MultiLabelBinarizer
 from sklearn.ensemble import RandomForestClassifier
+
+from imblearn.over_sampling import SMOTE, RandomOverSampler
 
 import xgboost as xgb
 from xgboost import XGBClassifier
@@ -31,11 +33,13 @@ pd.set_option('display.max_seq_items', None)
 
 
 # dtype={'type': str} prevents being confused with data type for large data sets
+"""
 train = pd.read_csv('data/general/train.csv', index_col='id', dtype={'type': str})
 val = pd.read_csv('data/general/val.csv', index_col='id', dtype={'type': str})
 test = pd.read_csv('data/general/test.csv', index_col='id', dtype={'type': str})
 train_translated = pd.read_csv('data/translated/train_translated.csv', dtype={'type': str})
 test_translated = pd.read_csv('data/translated/test_translated.csv', index_col='id', dtype={'type': str})
+"""
 
 combined_data = pd.read_csv('data/general/combined_data.csv', index_col='id', dtype={'type': str})
 combined_data_translated = pd.read_csv('data/translated/combined_data_translated.csv', index_col='id', dtype={'type': str})
@@ -44,23 +48,23 @@ combined_data_fully_translated = pd.read_csv('data/translated/combined_data_full
 prep = pd.read_csv('data/prepared_ready/prep.csv', index_col='id', dtype={'type': str})
 test_prep = pd.read_csv('data/prepared_ready/test_prepared.csv', index_col='id', dtype={'type': str})
 train_prep = pd.read_csv('data/prepared_ready/train_prepared.csv', index_col='id', dtype={'type': str})
-low = pd.read_csv('data/prepared_ready/prep_low_thres.csv', index_col='id', dtype={'type': str})
-high = pd.read_csv('data/prepared_ready/prep_high_thres.csv', index_col='id', dtype={'type': str})
+#low = pd.read_csv('data/prepared_ready/prep_low_thres.csv', index_col='id', dtype={'type': str})
+#high = pd.read_csv('data/prepared_ready/prep_high_thres.csv', index_col='id', dtype={'type': str})
 
 train_est_prepared = pd.read_csv('data/prepared_ready/train_est_prepared.csv', index_col='id', dtype={'type': str})
 val_est_prepared = pd.read_csv('data/prepared_ready/val_est_prepared.csv', index_col='id', dtype={'type': str})
 test_est_prepared = pd.read_csv('data/prepared_ready/test_est_prepared.csv', index_col='id', dtype={'type': str})
-train_est_smote100_03 = pd.read_csv('data/prepared_ready/train_est_smote100_03.csv', index_col='id', dtype={'type': str})
-train_est_smote100_full = pd.read_csv('data/prepared_ready/train_est_smote100_full.csv', index_col='id', dtype={'type': str})
+#train_est_smote100_03 = pd.read_csv('data/prepared_ready/train_est_smote100_03.csv', index_col='id', dtype={'type': str})
+#train_est_smote100_full = pd.read_csv('data/prepared_ready/train_est_smote100_full.csv', index_col='id', dtype={'type': str})
 
-train_en_prepared = pd.read_csv('data/prepared_ready/train_en_prepared.csv', index_col='id', dtype={'type': str})
-val_en_prepared = pd.read_csv('data/prepared_ready/val_en_prepared.csv', index_col='id', dtype={'type': str})
-test_en_prepared = pd.read_csv('data/prepared_ready/test_en_prepared.csv', index_col='id', dtype={'type': str})
+#train_en_prepared = pd.read_csv('data/prepared_ready/train_en_prepared.csv', index_col='id', dtype={'type': str})
+#val_en_prepared = pd.read_csv('data/prepared_ready/val_en_prepared.csv', index_col='id', dtype={'type': str})
+#test_en_prepared = pd.read_csv('data/prepared_ready/test_en_prepared.csv', index_col='id', dtype={'type': str})
 
-train_prepROS = pd.read_csv('data/prepared_ready/train_prepROS.csv')
-train_prepSMOTE = pd.read_csv('data/prepared_ready/train_prepSMOTE100.csv')
+#train_prepROS = pd.read_csv('data/prepared_ready/train_prepROS.csv')
+#train_prepSMOTE = pd.read_csv('data/prepared_ready/train_prepSMOTE100.csv')
 
-train_text = pd.read_csv('data/text/train_text.csv', index_col='id', dtype={'type': str})
-test_text = pd.read_csv('data/text/test_text.csv', index_col='id', dtype={'type': str})
+#train_text = pd.read_csv('data/text/train_text.csv', index_col='id', dtype={'type': str})
+#test_text = pd.read_csv('data/text/test_text.csv', index_col='id', dtype={'type': str})
 
 type_lookup = pd.read_csv('data/general/type_lookup.csv')
