b9583c1d5b34daad111c8e38479766d218c2cc16
Till Wenke <till.wenke@gmx.net>
2023-01-11 22:08:12 +0200

    text for cluster

--- setup_general.py
+++ setup_general.py
@@ -73,8 +73,10 @@ if os.popen('hostname').read() != 'till\n':
 
     train_text = pd.read_csv('data/text/train_text.csv', index_col='id', dtype={'type': str})
     test_text = pd.read_csv('data/text/test_text.csv', index_col='id', dtype={'type': str})
-    text = pd.read_csv('data/text/text.csv', index_col='id', dtype={'type': str})
     """
+    
+    text = pd.read_csv('data/text/text.csv', index_col='id', dtype={'type': str})
+    text_est = pd.read_csv('data/text/text_est.csv', index_col='id', dtype={'type': str})
 
     type_lookup = pd.read_csv('/gpfs/space/home/till/museum/museum_item_classification/data/general/type_lookup.csv')
 
